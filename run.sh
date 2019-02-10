ad5bb30c394fab3b8290495a82328af2b586401c
Giulio <giulio@localhost.localdomain>
2019-02-10 20:39:06 +0100

    fedora 29 gnome

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 sudo dnf install -y ansible
 
-ansible-playbook setup.yml -i HOSTS --ask-become-pass
+ansible-playbook setup.yml -i HOSTS --ask-become-pass -e 'ansible_python_interpreter=/usr/bin/python3'
